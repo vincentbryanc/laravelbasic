c6e50d18ebab79046ec0cdce21e39ee2fe59785e
Vincent Bryan Calija <vincentbryan.c@agentsofvalue.com>
2020-06-16 15:07:45 +0800

    delete unused code in routes

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return redirect(route('login'));
 });
 
-// Route::get('api/cities', 'CityController@cities');
 Route::apiResource('api/cities', 'API\CityController');
 
 Auth::routes(['register'=>false]);
